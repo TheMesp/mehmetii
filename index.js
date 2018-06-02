a59604b2c505e5a1745c726cdb0dd8fffb13fb84
TheMesp <webd@mts.net>
2018-06-01 21:55:07 -0500

    Added role checking

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ client.on('ready', () => {
 });
 client.on('message', message =>{
     //filter out the weak.
-    if((!message.content.startsWith(prefix)&&message.content === 'Byzantium is good') || message.author.bot) return;
+    if((!message.content.startsWith(prefix)&&!message.content === 'Byzantium is good') || message.author.bot) return;
     const args = message.content.slice(prefix.length).split('/ +/');//Turns the textline into an array of args.
     const command = args.shift().toLowerCase();//returns the first argument (the command) and then removes it from the array.
+
+    //defines "admin" as the role that can access advanced bot commands.
+    let admin = message.guild.roles.find("name", "Game Coordinator");
+
     //prints the message to the console.
     console.log(message.author.username + ' said: ' + message.content);
     //Special case - no prefix required for mehmet delivering cold hard truth.
@@ -27,9 +31,13 @@ client.on('message', message =>{
     else if(command === 'join'){
         
     }
-    else if(command === 'prune'){
-        if(args.size != 0 && message.member.roles.find("name", "Game Coordinator")){
-            message.channel.bulkDelete(args[0],true);
+    //Bossman commands
+    else if(message.member.roles.has(admin.id)){
+        console.log("bossman said something");
+        if(command === 'prune'){
+            if(args.size != 0){
+                message.channel.bulkDelete(args[0],true);
+            }
         }
     }
 })
